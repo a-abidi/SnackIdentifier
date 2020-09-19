0b7ddc0f82e68279c18361f65c26c409e26f99e2
a-abidi <aliabidi@icloud.com>
2020-09-19 18:34:22 +0100

    Add Camera access functionality.

--- SnackIdentifier/ViewController.swift
+++ SnackIdentifier/ViewController.swift
@@ -11,11 +11,20 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera // Implementing camera functionality
+        imagePicker.allowsEditing = false
     }
-
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    }
 
 }
 
